78fe6401ccbc9ae55ea7c40f95d54454ee8720ec
JahnieTurner <80420943+JahnieTurner@users.noreply.github.com>
2022-05-19 16:48:39 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 music1 = "";
 music2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload() {
     music1 = loadSound("music.mp3");
@@ -11,8 +15,27 @@ function setup() {
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY.results[0].pose.leftWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+
+
+    }
+}
+
+function modelLoaded() {
+    console.log("poseNet is initialized");
 }
 
 function draw() {
-    image(video, 0, 0, 500, 500)
+    image(video, 0, 0, 500, 500);
 }
